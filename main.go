67f062e55a7c1f21cf7878dddf4bda55126edbb8
Varun Sood <soodvarun2003@gmail.com>
2025-03-08 11:02:49 +0530

    Added REST API endpoints using gin

--- main.go
+++ main.go
@@ -4,24 +4,29 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"sync"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Block struct {
-	Index        int
-	TimeStamp    string
-	Transactions []string
-	PrevHash     string
-	Hash         string
-	Proof        int
+	Index        int      `json:"index"`
+	Timestamp    string   `json:"timestamp"`
+	Transactions []string `json:"transactions"`
+	PrevHash     string   `json:"prev_hash"`
+	Hash         string   `json:"hash"`
+	Proof        int      `json:"proof"`
 }
 
 type Blockchain struct {
-	Chain []Block
+	Chain       []Block
+	PendingTxns []string
+	mu          sync.Mutex
 }
 
 func CalculateHash(block Block) string {
-	record := fmt.Sprintf("%d%s%v%s%d", block.Index, block.TimeStamp, block.Transactions, block.PrevHash, block.Proof)
+	record := fmt.Sprintf("%d%s%v%s%d", block.Index, block.Timestamp, block.Transactions, block.PrevHash, block.Proof)
 	hash := sha256.Sum256([]byte(record))
 	return hex.EncodeToString(hash[:])
 }
@@ -29,7 +34,7 @@ func CalculateHash(block Block) string {
 func (bc *Blockchain) CreateGenesisBlock() {
 	genesisBlock := Block{
 		Index:        0,
-		TimeStamp:    time.Now().String(),
+		Timestamp:    time.Now().Format(time.RFC3339),
 		Transactions: []string{"Genesis Block"},
 		PrevHash:     "",
 		Proof:        100,
@@ -55,47 +60,57 @@ func ProofOfWork(lastProof int) int {
 	return proof
 }
 
-func (bc *Blockchain) AddBlock(transactions []string) {
+func (bc *Blockchain) MineBlock() Block {
+	bc.mu.Lock()
+	defer bc.mu.Unlock()
+
 	lastBlock := bc.GetLastBlock()
 	newProof := ProofOfWork(lastBlock.Proof)
 
 	newBlock := Block{
 		Index:        lastBlock.Index + 1,
-		TimeStamp:    time.Now().String(),
-		Transactions: transactions,
+		Timestamp:    time.Now().Format(time.RFC3339),
+		Transactions: bc.PendingTxns,
 		PrevHash:     lastBlock.Hash,
 		Proof:        newProof,
 	}
 	newBlock.Hash = CalculateHash(newBlock)
 	bc.Chain = append(bc.Chain, newBlock)
+	bc.PendingTxns = []string{}
+	return newBlock
 }
 
-func (bc *Blockchain) IsValid() bool {
-	for i := 1; i < len(bc.Chain); i++ {
-		prevBlock := bc.Chain[i-1]
-		currBlock := bc.Chain[i]
-
-		if currBlock.Hash != CalculateHash(currBlock) {
-			return false
-		}
-		if currBlock.PrevHash != prevBlock.Hash {
-			return false
-		}
-	}
-	return true
+func (bc *Blockchain) AddTransaction(txn string) {
+	bc.mu.Lock()
+	defer bc.mu.Unlock()
+	bc.PendingTxns = append(bc.PendingTxns, txn)
 }
 
 func main() {
+	r := gin.Default()
 	blockchain := Blockchain{}
 	blockchain.CreateGenesisBlock()
 
-	blockchain.AddBlock([]string{"Varun sent Aryan 5 BTC"})
-	blockchain.AddBlock([]string{"Rahul sent Varun 5 BTC"})
+	r.GET("/chain", func(c *gin.Context) {
+		c.JSON(200, gin.H{"chain": blockchain.Chain})
+	})
 
-	fmt.Println("Blockchain valid:", blockchain.IsValid())
+	r.POST("/transactions", func(c *gin.Context) {
+		var req struct {
+			Transaction string `json:"transaction"`
+		}
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(400, gin.H{"error": "Invalid request"})
+			return
+		}
+		blockchain.AddTransaction(req.Transaction)
+		c.JSON(200, gin.H{"message": "Transaction added"})
+	})
 
-	for _, block := range blockchain.Chain {
-		fmt.Printf("\nIndex: %d\nTimestamp: %s\nTransactions: %v\nPrevHash: %s\nHash: %s\nProof: %d\n",
-			block.Index, block.TimeStamp, block.Transactions, block.PrevHash, block.Hash, block.Proof)
-	}
+	r.GET("/mine", func(c *gin.Context) {
+		newBlock := blockchain.MineBlock()
+		c.JSON(200, gin.H{"message": "Block Mined!", "block": newBlock})
+	})
+
+	r.Run(":8080")
 }
